7ac74a06bbff6463791f3b3db78d3e81a229e66f
Damaris Poppe <damariscecilia29@gmail.com>
2025-09-28 17:36:06 -0600

    simple route setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
+// server side Javascript
+// Express.js runs on Node.js
 const express = require('express');
-const app = express();
+
+const app = express(); // instance of express
 const port = 3000;
 
-// Route for the root URL ('/')
-app.get('/', (req, res) => {
-    res.send("Hello World");
+//HTML verbs: GET (-data), POST (create), PUT (update), DELETE (-data)
+
+// Route are the enpoints of the server, Routes respond to client requests. 
+// Route for the root URL ('/') home page
+app.get('/hello', (req, res) => {
+    res.set('Content-type', 'text/html');
+    res.status(200).send("Hello World");
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Example app listening on port ${port}`);
+    }
+    else {
+        console.log('Error occurred, server can\'t start', error);
+    }
 })
