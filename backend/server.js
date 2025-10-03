f6e998b6ed0a068a4cbedbda8bee800567cd6a28
Damaris Poppe <damariscecilia29@gmail.com>
2025-10-03 00:11:14 -0600

    upsted: server.js handles server health route, use other routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,31 @@
 // server side Javascript
 // Express.js runs on Node.js
 const express = require('express');
+const express = require('cors');
 
 const app = express(); // instance of express
 const port = 3000;
 
-//HTML verbs: GET (-data), POST (create), PUT (update), DELETE (-data)
+// Middleware
+app.use(cors());
+app.use(express.json());
 
+//HTML verbs: GET (-data), POST (create), PUT (update), DELETE (-data)
 // Route are the enpoints of the server, Routes respond to client requests. 
-// Route for the root URL ('/') home page
-app.get('/hello', (req, res) => {
-    res.set('Content-type', 'text/html');
-    res.status(200).send("Hello World");
-})
+
+// health check route
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'ok',
+        timestamp: new Date().toISOString,
+        service: 'Inventory API'
+    });
+});
+
+// Routes
+// product route
+const productRoutes = require('./routes/products.js');
+app.use('/api/products', productRoutes);
 
 app.listen(port, (error) => {
     if (!error) {
